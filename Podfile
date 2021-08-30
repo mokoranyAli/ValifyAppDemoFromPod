a2e4b6f241db4b43881a26b3808a8a3bfe1f5a9f
Mohamed Korany <>
2021-08-30 17:30:48 +0200

    Add missing files

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ValifyDemoFromPod' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ValifyDemoFromPod
+pod 'ValifySelfie', '0.1.1'
+end
--- ValifyDemoFromPod/ViewController.swift
+++ ValifyDemoFromPod/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import ValifySelfie
 
+// MARK: - ViewController
+//
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  
+  // MARK: - IBOutlets
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+  // MARK: - IBActions
+  
+  @IBAction func showValifyTapped(_ sender: Any) {
+    showValify(from: self, with: .present)
   }
-
-
 }
 
+// MARK: - ValifyDelegate
+//
+extension ViewController: ValifySelfieDelegate {
+  
+  func didFinishScan(from viewController: UIViewController, with result: Result<UIImage, Error>) {
+    viewController.dismiss(animated: true)
+    let image = try? result.get()
+    self.imageView.image = image?.withHorizontallyFlippedOrientation()
+  }
+}
