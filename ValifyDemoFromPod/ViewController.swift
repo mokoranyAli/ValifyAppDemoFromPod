35e099a0446026e8a06af430e6c2d3344ca84ae3
Mohamed Korany <>
2021-08-30 17:30:13 +0200

    first commit

---
+++ ValifyDemoFromPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValifyDemoFromPod
+//
+//  Created by Mohamed Korany on 30/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
